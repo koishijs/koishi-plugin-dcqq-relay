f14ba0be9edd99a49edf17e61892ecdca520e730
LittleC <i@ltlec.cn>
2021-03-18 19:47:32 +0800

    :zap: 新embed支持

--- src/index.ts
+++ src/index.ts
@@ -148,17 +148,18 @@ const adaptMessage = async (meta: Session.Payload<"message", any>) => {
       return `@${v.data.role || v.data.id}`
     } else if (v.type === "share") {
       return v.data?.title + ' ' + v.data.url
-    } else if (v.type === 'embed') {
-      let embed = JSON.parse(v.data.data) as Embed
-      let rtn = ''
-      rtn += embed.description || ''
-      embed.fields?.forEach(field => {
-        rtn += `${field.name}: ${field.value}\n`
-      })
-      return rtn
     }
     return segment.join([v]).trim()
   }).join('')
+  contents = meta.discord?.embeds?.map(embed => {
+    let rtn = ''
+    rtn += embed.description || ''
+    embed.fields?.forEach(field => {
+      rtn += `${field.name}: ${field.value}\n`
+    })
+    return rtn
+  }) + contents
+  
   let quotePrefix = ""
   let quoteObj: MessageRelation | null;
   if (meta.quote) {
